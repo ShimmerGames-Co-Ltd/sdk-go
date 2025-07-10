8de7eef03e520aefd1e98f06545a2e8b1a78f029
neuronwade <neuronwade@gmail.com>
2025-07-10 14:17:43 +0800

    feat(user): 服务器验证玩家支持ip地址信息返回

--- services/authorization/models.go
+++ services/authorization/models.go
@@ -16,10 +16,12 @@ type (
 	// RoleId 平台角色ID, 当前应用下唯一ID
 	// ShortId 平台角色短ID, 当前应用下唯一ID
 	// DispatchServer 用于服务器分发(未接入可忽略)
+	// RemoteAddr 用户访问IP地址
 	UserAuthorizeResponse struct {
 		UserId         int64  `json:"user_id"`
 		RoleId         int64  `json:"role_id"`
 		ShortId        int32  `json:"open_id"` // Alias: 平台OpenID 等同于 角色短ID
 		DispatchServer string `json:"dispatch_server"`
+		RemoteAddr     string `json:"remote_addr"`
 	}
 )
